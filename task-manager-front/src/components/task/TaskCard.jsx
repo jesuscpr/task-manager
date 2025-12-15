8f63791e9a3877361045d92f04315bd480cb025f
Jesús <jesusperezrobles21@gmail.com>
2025-12-15 22:23:24 +0100

    TaskCard content and style

--- task-manager-front/src/components/task/TaskCard.jsx
+++ task-manager-front/src/components/task/TaskCard.jsx
@@ -1,12 +1,24 @@
 import './TaskCard.css'
 
+const priorityColors = {
+    'LOW': '#14b451',
+    'MEDIUM': '#3498db',
+    'HIGH': '#f39c12',
+    'URGENT': '#e74c3c'
+  }
+
 const TaskCard = ({ title, task, isDragging, onTaskClick }) => {
   return (
     <article
       className={`taskCard ${isDragging ? 'dragging' : ''}`}
       onClick={() => onTaskClick && onTaskClick(task)}
+      style={{ border: `2px solid ${priorityColors[task.priority]}` }}
     >
       <p className='taskText'>{title}</p>
+      
+      {task.assigned_to[0]?.username && (
+        <p className='taskAssigned'>{task.assigned_to[0]?.username}</p>
+      )}
     </article>
   )
 }
